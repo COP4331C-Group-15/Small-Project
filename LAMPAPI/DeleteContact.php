2239b72f1e103200c3ac5c651b254d4794f59562
Karlos Morales <83847661+Karlosm012@users.noreply.github.com>
2023-08-31 13:38:54 -0400

    Added deleteContact PHP file

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,38 @@
+<?php
+    $inData = getRequestInfo();
+
+    $userId = $inData["userId"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+    if ($conn->connect_error) {
+        returnWithError ($conn->connect_error);
+    }
+
+    else {
+        // we need all the correct information to be able to delete 
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ?");
+        $stmt->bind_param("ssi", $firstName, $lastName, $userId);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
+
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input', true));
+    }
+
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err) {
+        $retValue = '{"error":"' . $err > '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    ?>
