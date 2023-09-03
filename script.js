d0993074bde24fb6af03969d18674f8e982c683f
Joshua Currie <johndoe@example.com>
2023-09-02 23:09:08 -0400

    initalized fields in javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+// initalize fields from HTML elements
+
+// from index.html
+let loginUsername = document.getElementById('login-username');
+let loginPassword = document.getElementById('login-password');
+let loginButton = document.getElementById('login-button');
+
+// from signup.html
+let signupFirstName = document.getElementById('firstname');
+let signupLastName = document.getElementById('lastname');
+let signupUsername = document.getElementById('signup-username');
+let signupPassword = document.getElementById('signup-password');
+let signupReenterPassword = document.getElementById('reenter-password');
+let signupButton = document.getElementById('signup-button');
+
+// from contacts.html
+let createContactFirstname = document.getElementById('create-contact-firstname');
+let createContactLastname = document.getElementById('create-contact-lastname');
+let createContactEmail = document.getElementById('create-contact-email');
+let createContactPhone = document.getElementById('create-contact-phone');
+let addContactButton = document.getElementById('addd-contact-button');
+let logoutButton = document.getElementById('logout-button');
+let searchBarInput = document.getElementById('searchbar');
+let searchButton = document.getElementById('search-contacts-button');
+// Josh: I will add refresh button here eventually 
