665f51a92ed09a25ba373b224b77ec162cb207f0
SSGrady <gradystevena@gmail.com>
2023-09-03 17:00:42 -0400

    Added SQL functionality to SearchByUser.php

--- LAMPAPI/SearchByUser.php
+++ LAMPAPI/SearchByUser.php
@@ -10,4 +10,52 @@
 	{
 		returnWithError( $conn->connect_error );
 	}
+    else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login= ?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+
+		$queriedResult = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			$queryCount++;
+		}
+
+		if( $queryCount == 0 )
+		{
+			returnWithInfo( "" );
+		}
+		else
+		{
+			returnWithError( "Username has been taken" );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"Error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $info )
+	{
+		$retValue = '{"Error": "' . $info . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
