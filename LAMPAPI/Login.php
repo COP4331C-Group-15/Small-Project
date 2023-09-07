1dc8daf7bf75cb0df37bae7324605323a113545f
Joshua Currie <johndoe@example.com>
2023-09-07 15:13:11 -0400

    added headers to login and registration php (test)

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,6 +1,11 @@
 
 <?php
 
+	// Enable CORS for all domains (not recommended for production)
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 	$inData = getRequestInfo();
 	
 	$id = 0;
