12b322917a24629573361b19e38b876a9ec14993
Joshua Currie <johndoe@example.com>
2023-09-08 10:46:13 -0400

    cleaned up javascript file (move functions around)

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ function doLogin()
 	
 				window.location.href = "contacts.html";
 
-				document.getElementById("firstname-contact-page-message").innerHTML = "firstName";
-				document.getElementById("lastname-contact-page-message").innerHTML = "lastName";
+				// document.getElementById("firstname-contact-page-message").innerHTML = "firstName";
+				// document.getElementById("lastname-contact-page-message").innerHTML = "lastName";
 			}
 		};
 		xhr.send(jsonPayload);
@@ -65,49 +65,6 @@ function doLogin()
 	catch(err)
 	{
 		document.getElementById("login-result").innerHTML = err.message;
-	}
-
-}
-
-function saveCookie()
-{
-	let minutes = 20;
-	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-}
-
-function readCookie()
-{
-	userId = -1;
-	let data = document.cookie;
-	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		let thisOne = splits[i].trim();
-		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
-	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
-	else
-	{
-		// changed later
-		// document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
 }
 
@@ -148,7 +105,6 @@ function doRegister()
         
                 if( userId < 1 )
                 {       
-                    // document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
           			console.log("Registration failed");
                     return;
                 }
@@ -168,6 +124,49 @@ function doRegister()
     catch(err)
     {
     	console.log("Registration failed");
-        // document.getElementById("login-result").innerHTML = err.message;
     }
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		// changed later
+		//document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+		document.getElementById("firstname-contact-page-message").innerHTML = firstName;
+		document.getElementById("lastname-contact-page-message").innerHTML = lastName;
+	}
 }
